57068c331b920ceb3ee9a3897613dd753e40d59b
Filip Heitbrink <info@interactive360.eu>
2014-06-30 17:04:00 +0200

    Created initial CI templates and navigation menu

---
+++ application/views/block.menu.php
@@ -0,0 +1,27 @@
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Zoho Books - Bridge</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+            <ul class="nav nav-sidebar">
+                <li class="active"><a href="#">Dashboard</a></li>
+                <li><a href="<?php site_url('banktransactions'); ?>">Import bank transactions</a></li>
+            </ul>
+        </div>
