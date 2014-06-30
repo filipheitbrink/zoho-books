97cb9a73d6730a2517d6bb7fac3844300a7e9556
Filip Heitbrink <info@interactive360.eu>
2014-06-30 16:50:24 +0200

    Installed Bootstrap plus initial dashboard template

--- application/views/home.php
+++ application/views/home.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="css/dashboard.css" rel="stylesheet">
+    <link href="/css/dashboard.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
